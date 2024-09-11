d18c3da95ca6bb0761a6da6830d43d37c92738c9
mitsy01 <alexanderfigmochka19@gmail.com>
2024-09-11 23:26:46 +0300

    change cod

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 from flask import Flask, render_template
 
+import random
+
 
 app = Flask(__name__)
 
 
 @app.get("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="")
 
 
-@app.get("/menu/")
+@app.get("/menu-baikaino/")
 def menu():
-    return render_template("menu.html")
+    pizzas = [
+        {"name": "Суйгетсу", "price": 50, "ingredients": "ковбаса, сир, зелень, соус тартар"},
+        {"name": "Моцарела", "price": 56, "ingredients": "сир, соус, зелень"},
+        {"name": "Чотири ковбаси", "price": 47, "ingredients": "ковбаса 'Перероні', ковбаса 'Мадер', ковбаса 'Тульчин', ковбаса 'Бандерівська'"},
+        {"name": "Хічіго", "price": 69, "ingredients": "полуниця, ананас, шоколад, зефір"}
+    ]
+    countext = {
+        "pizzas": pizzas,
+        "title": "Шикайне меню"
+    }
+    return render_template("menu.html", **countext)
 
 
 @app.get("/contacts/")
 def contacts():
-    return render_template("contacts.html")
+    context = {
+        "first_number": random.randint(101, 999),
+        "second_number": random.randint(1001, 9999)
+    }
+    return render_template("contacts.html", **context)
 
 
 if __name__ == "__main__":
